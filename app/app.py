90f6765c69c51937384d39dbffe8d9245aaf626e
valentinCord <valentin.corduant@gmail.com>
2022-12-08 15:26:16 +0100

    pacman and webpage

--- app/app.py
+++ app/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
